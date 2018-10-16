39f4be51b9b96e4c3ed295b90c9330ee624c1981
Gustavo Pires de Moraes <gustavopmoraes@gmail.com>
2018-10-16 08:20:04 -0300

    Changes

---
+++ src/main/java/com/gpmoraes/gotcamel/GotcamelApplication.java
@@ -0,0 +1,12 @@
+package com.gpmoraes.gotcamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotcamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotcamelApplication.class, args);
+	}
+}
