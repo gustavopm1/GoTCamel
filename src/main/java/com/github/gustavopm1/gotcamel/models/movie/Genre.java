c3ab2be0c1b2faedd9b98b25c1de81b1605c9c1a
Gustavo Pires de Moraes <gustavopmoraes@gmail.com>
2018-10-19 16:20:50 -0300

    Movie Route, Movie Cast Route and Person Route tests.

---
+++ src/main/java/com/github/gustavopm1/gotcamel/models/movie/Genre.java
@@ -0,0 +1,11 @@
+package com.github.gustavopm1.gotcamel.models.movie;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Genre {
+    Integer id;
+    String name;
+}
--- src/main/java/com/github/gustavopm1/gotcamel/models/movie/Movie.java
+++ src/main/java/com/github/gustavopm1/gotcamel/models/movie/Movie.java
@@ -3,9 +3,19 @@ package com.github.gustavopm1.gotcamel.models.movie;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Date;
+import java.util.List;
+
 @Data
 @Builder
 public class Movie {
-    String name;
+    String original_title;
+    String overview;
     int year;
+    Date release_date;
+    Integer runtime;
+    List<Genre> genres;
+    List<MovieKeyword> keywords;
+    List<MovieCast> cast;
+    List<Crew> crew;
 }
